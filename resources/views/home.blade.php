b56e55fee901b36a9d2482e0fa79edda3a40edba
Rajibcse8 <rajibtalukder119@gmail.com>
2021-06-09 00:13:39 +0600

    little error fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,38 +24,6 @@
     </div>
 
 
-    {{-- <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div> --}}
-
-    {{-- <div class="container">
-    <div class="row justify-content-center">
-        <form action="{{ route('album.store') }}" method="POST" enctype="multipart/form-data">
-            @csrf
-            <input type="text" name="album" class="form-control" placeholder="Enter your album name">
-            <input type="file" name="image[]" class="form-control">
-            <input type="file" name="image[]" class="form-control">
-            <input type="file" name="image[]" class="form-control">
-            <button class="btn btn-primary" types="submit">Submit</button>
-
-        </form>
-    
-    </div>
-    @foreach ($images as $image)
-    <img srcset="{{ asset('storage/'.$image->name) }}"  class="img-thumbnail`">
-       
-   @endforeach
-</div> --}}
 
 @endsection
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" referrerpolicy="no-referrer"></script>
@@ -69,10 +37,12 @@
           
         });
 
-         $(".body").on("click",".btn-remove",function(){
+     
+
+         $("body").on("click",".btn-remove",function(){
            
            $(this).parents(".control-group").remove();
-         })
+         });
 
     });
 
